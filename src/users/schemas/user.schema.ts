eaa40d900fd0ec8a53d6acbef616d6ad0db0f8f3
vbthang <2151062875@e.tlu.edu.vn>
2025-01-19 20:57:55 +0700

    feat: add user schema

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -3,13 +3,40 @@ import { HydratedDocument } from 'mongoose';
 
 export type UserDocument = HydratedDocument<User>;
 
-@Schema()
+@Schema({ timestamps: true })
 export class User {
   @Prop()
   name: string;
 
   @Prop()
-  age: number;
+  email: string;
+
+  @Prop()
+  password: string;
+
+  @Prop()
+  phone: string;
+
+  @Prop()
+  address: string;
+
+  @Prop()
+  avatar: string;
+
+  @Prop()
+  accountType: string;
+
+  @Prop()
+  role: string;
+
+  @Prop()
+  status: string;
+
+  @Prop()
+  codeId: string;
+
+  @Prop()
+  codeExpired: Date;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
